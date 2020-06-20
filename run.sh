4575a4c701f2d33058441d8ba34008c1b4691082
Szymon Richert <szymon.rychu@gmail.com>
2020-06-20 19:20:39 +0200

    Add freenect installation

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ which python3 2> /dev/null || sudo apt-get install python3 -y
 which pip3 2> /dev/null    || sudo apt-get install python3-pip -y
 which ansible 2> /dev/null || sudo pip3 install ansible
 
-sudo ansible-playbook -i localhost playbook.yaml
+ sudo ansible-playbook playbook.yaml
